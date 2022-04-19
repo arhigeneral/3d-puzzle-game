0ea2da41d9a790b73fad0aae52616a6f450a83a0
Your Name <sad_casd@mail.ru>
2022-04-19 17:37:04 +0300

    New mech button add

--- Assets/player.cs
+++ Assets/player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
 public class player : MonoBehaviour
 {
@@ -17,6 +18,42 @@ public class player : MonoBehaviour
         if (Input.GetKey(keyTwo))
         {
             GetComponent<Rigidbody>().velocity -= moveDirection;
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(this.CompareTag("Player") && other.CompareTag("Finish"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        if (this.CompareTag("Cube") && other.CompareTag("Cube"))
+        {
+            foreach(Activator button in FindObjectsOfType<Activator>())
+            {
+                button.canPush = false;
+            }
+        }
+    }
+        private void OnTriggerExit(Collider other)
+    {
+        if(this.CompareTag("Player") && other.CompareTag("Finish"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        if (this.CompareTag("Cube") && other.CompareTag("Cube"))
+        {
+            foreach(Activator button in FindObjectsOfType<Activator>())
+            {
+                button.canPush = true;
+            }
         }
     }
 }
